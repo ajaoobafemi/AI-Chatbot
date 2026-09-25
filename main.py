c3abc2e9e959b5cfb41ff813fcf452c9a629e31c
ajaoobafemi <ajaoobafemi9@gmail.com>
2026-09-26 00:41:02 +0100

    Phase 2: persistent per-user memory using JSON file

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+import json
+import os
 import random
 responses = {
     "hello": ["Hi there!", "Hey!", "Hello! How can I help?"],
     "how are you": ["I'm doing great, thanks!", "I'm just a bot, but I'm good!"],
 }
-user_name = None
+username = input("Enter your username: ")
+filename = f"memory_{username}.json"
+
+if os.path.exists(filename):
+  with open(filename, 'r') as f:
+    data = json.load(f)
+    name = data["name"]
+    print(f"Bot: Welcome back {name}!")
+else:
+  username = None
+  print("Bot: Welcome! I don`t think we`ve met.")
+
 while True:
   user = input("You: ").lower()
   if "my name is" in user:
     parts = user.split("my name is")
     user_name = parts[1].strip().capitalize()
     print(f"Bot: Nice to meet you {user_name}!")
+    with open(filename, 'w') as f:
+       json.dump({"name": user_name}, f)
     continue
   elif 'bye' in user:
     print("Bot: Goodbye!")
